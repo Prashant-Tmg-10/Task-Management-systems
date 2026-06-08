ce1a715d7337ffc17e47cc8396710392d63d5f35
Prashant-Tmg-10 <prashantxxtmg@gmail.com>
2026-06-08 18:26:24 +0545

    fix vercel deployment

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from fastapi import FastAPI
 from src.utils.db import Base, engine
-#from src.tasks.models import TaskModel
 from src.tasks.router import task_routes
 from src.user.router import user_routes
 from fastapi.middleware.cors import CORSMiddleware
 from src.utils.settings import settings
 
-
+# create tables
 Base.metadata.create_all(bind=engine)
 
-app=FastAPI(title="This is my Task Management System")
+app = FastAPI(title="Task Management System")
+
+# routers
 app.include_router(task_routes)
 app.include_router(user_routes)
 
+# CORS setup
 frontend_origins = [
     origin.strip()
     for origin in settings.FRONTEND_URL.split(",")
     if origin.strip()
 ]
 
-# Allow frontend (React) to talk to backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=frontend_origins,
@@ -27,4 +28,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
